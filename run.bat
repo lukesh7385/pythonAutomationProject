b00e4e38da630b0e33542ef2396f7a6220e6a700
lukesh <adelukesh@gmail.com>
2023-08-09 14:44:04 +0530

    update

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-rem pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
+pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
 rem pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser firefox
-pytest -s -v -m "sanity or regression" --html=./Reports/report.html testCases/ --browser chrome
+rem pytest -s -v -m "sanity or regression" --html=./Reports/report.html testCases/ --browser chrome
 rem pytest -s -v -m "sanity and regression" --html=./Reports/report.html testCases/ --browser chrome
 rem pytest -s -v -m "regression" --html=./Reports/report.html testCases/ --browser chrome
